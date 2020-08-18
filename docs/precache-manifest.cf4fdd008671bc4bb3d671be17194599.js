9eb6be6021618eda6dfbcb6eb46f4019330e110c
Edward Fuentes <edwarfuentes972@gmail.com>
2020-08-18 18:57:56 -0500

    Docs agregado 2

---
+++ docs/precache-manifest.cf4fdd008671bc4bb3d671be17194599.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a1f867ba9442758295ba603b63e40d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c21aaa9a1663bf140d",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "bf548dba832733147d28",
+    "url": "/static/css/main.c9e81368.chunk.css"
+  },
+  {
+    "revision": "a2c21aaa9a1663bf140d",
+    "url": "/static/js/2.8d6e07df.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.8d6e07df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf548dba832733147d28",
+    "url": "/static/js/main.a420bda9.chunk.js"
+  },
+  {
+    "revision": "98b05610af92de109cdc",
+    "url": "/static/js/runtime-main.082e128f.js"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
